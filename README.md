ae4455c17676ecafc5ab5f247c4c89e7d45ea034
Morpeh-O <oleg.bilik1994@gmail.com>
2017-04-23 19:29:25 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webpack2-js-boilerplate
 Webpack 2 setup for fast start a new JavaScript project
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](http://standardjs.com)
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const develop = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+
+  entry: {
+    app: './app.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  },
+
+  devtool: develop ? 'cheap-module-source-map' : false,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pcss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader'
+          ]
+        })
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true,
+      disable: develop
+    })
+  ]
+}
